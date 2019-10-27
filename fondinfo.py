1aa1311c8db743b27d8f55b71df41f60a35771e6
aleksols <aleko@stud.ntnu.no>
2019-10-27 12:46:17 +0100

    Add files

---
+++ fondinfo.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
+
+
+class Fondinfo:
+
+    def __init__(self):
+        # self.load_funds()
+        self.driver = webdriver.Chrome("/usr/local/bin/chromedriver")
+
+        self.my_funds = {}
+
+    def load_funds(self):
+        raise NotImplementedError
+
+    def get_page_content(self, url):
+        self.driver.get(url)
+        time.sleep(5)
+        content = self.driver.page_source
+        soup = BeautifulSoup(content)
+        return soup
+
+    def add_fund(self, fund_name, amount, invested, url):
+        if fund_name not in self.my_funds.keys():
+            self.my_funds[fund_name] = amount, invested, url
+        else:
+            self.my_funds[fund_name][0] += amount
+            self.my_funds[fund_name][1] += invested
+
+    def calculate_total_earning(self):
+        earnings = 0
+        for fund, values in self.my_funds.items():
+            page_content = self.get_page_content(values[2])
+            price = page_content.find("div", attrs={"class": "number LAST"}).text
+            price = price.replace("\xa0", "").replace(",", ".")
+            price = float(price)
+            earnings += price * values[0] - values[1]
+        return earnings
+
+
+if __name__ == '__main__':
+    info = Fondinfo()
+    info.add_fund("Storebrand Indeks - Alle Markeder", 8.054933, 20000,
+                  "https://bors.e24.no/#!/instrument/SP-IDXAM.OSE")
+    info.add_fund("KLP AKSJE VERDEN INDEKS", 3.654736, 10000, "https://bors.e24.no/#!/instrument/KL-AVIND.OSE")
+    info.add_fund("Storebrand Indeks - Norge", 	13.456056, 20000, "https://bors.e24.no/#!/instrument/SP-INDNO.OSE")
+    earnings = info.calculate_total_earning()
+    print(earnings)
+    info.driver.close()
